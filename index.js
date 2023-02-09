8abcc01c9899e2098ed7e6c1ff7e72dfff51c5a8
Amanda <iulianoamanda@gmail.com>
2023-02-08 20:29:16 -0600

    added src

---
+++ index.js
@@ -0,0 +1,59 @@
+let now = new Date();
+let h3 = document.querySelector("h3");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+h3.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+function showPosition(position) {
+  let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+//let cityName = document.querySelector("#city");
+//let searchInput = document.querySelector("#city-input");
+
+// cityName.innerHTML = `${searchInput.value}`;
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let button = document.querySelector("#current-location-btn");
+button.addEventListener("click", getCurrentLocation);
+
+search("New York");
